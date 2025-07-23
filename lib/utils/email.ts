d1395c7ac1565d27483f129ffbbb3a4fef779e1d
Arpit Verma <arpitv970@gmail.com>
2025-07-23 08:34:20 +0530

    updated env.example

--- lib/utils/email.ts
+++ lib/utils/email.ts
@@ -3,7 +3,7 @@ import { createTransport } from "nodemailer";
 const transporter = createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, 
+  secure: process.env.SMTP_SECURE === "true", 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
